c69a2a91c880e67fab5436526e23a93739b6da4d
robinshiji <robinshiji3@gmail.com>
2024-10-20 14:32:42 +0530

    project complete

--- pc_app/views.py
+++ pc_app/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.shortcuts import redirect,render
-from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from .models import *
-from django.shortcuts import HttpResponse,get_object_or_404
+from django.shortcuts import get_object_or_404
 
 
 #home page
